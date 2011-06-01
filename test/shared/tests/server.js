2a44fb4a27c5e0be70932986259e04b62c926cdb
Elijah Insua <tmpvar@gmail.com>
2011-05-31 21:59:43 -0400

    enough for a basic server

--- test/shared/tests/server.js
+++ test/shared/tests/server.js
@@ -6,11 +6,35 @@ var noop = function() {};
 
 module.exports = {
   'SANITY: test tick rate' : function(t) {
-    var handle = motion.server(1, noop, noop, function(tick, patches) {
+    var handle = motion.server(1, noop, function(tick, patches) {
       t.ok(tick === 1);
       t.ok(patches.length === 0);
       handle.stop();
       t.done();
     });
+  },
+ 'test merging' :  function(t) {
+    var  data   = {}
+
+    var handle = motion.server(1, 
+      function apply(patches, fn) {
+        var i=0, l=patches.length, patch;
+        for (i; i<l; i++) {
+          patch = patches[i];
+          data[patch.key] = patch.value;
+        }
+        fn([], patches);
+      }, function(tick, patches) {
+        if (patches.length > 0) {
+          t.ok(patches[0].key === 'abc');
+          handle.stop();
+          t.done();
+        }
+    });
+
+    handle.add([{
+      key   : 'abc',
+      value : 123
+    }]);
   }
 };
